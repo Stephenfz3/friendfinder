aa92f79fe74f1ba488c12a2d6e16ab51a68db732
Stephen <stephenf3@yahoo.com>
2019-09-14 23:42:26 -0400

    rough draft of this project, not completed

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Excision',
+		photo: 'https://www.billboard.com/files/media/Excision-Press-photo-2018-billboard-1548.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Chibs',
+		photo: 'http://outlawartistagency.com/____impro/1/onewebmedia/180830_NANGGANG-42_edited1.jpg?etag=W%2F%22130f39-5ba1014d%22&sourceContentType=image%2Fjpeg&ignoreAspectRatio&resize=536%2B536&extract=0%2B0%2B525%2B536&quality=85',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Illenium',
+		photo: 'https://yt3.ggpht.com/a/AGF-l78MTn-gA6A26YQOFqfzywGYXpY_7sl9hBQLLA=s900-c-k-c0xffffffff-no-rj-mo',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Dubloadz',
+		photo: 'https://everipedia-storage.s3.amazonaws.com/ProfilePics/dubloadz__62079.jpeg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Dion Timmer',
+		photo: 'https://ukf-cdn-2ezlhsfwy1f.stackpathdns.com/wp-content/uploads/2017/03/DION-PRESS-WEB-BY-FADEWOOD-STUDIOS-1024x1024.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: '12th Planet',
+		photo: 'https://images.sk-static.com/images/media/profile_images/artists/2070755/huge_avatar',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'He$h',
+		photo: 'http://www.fouroverfour.jukely.com/wp-content/uploads/2018/06/q0y33caH-300x300.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Svdden Death',
+		photo: 'https://i1.sndcdn.com/avatars-000650966727-1qh14s-t500x500.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ghastly',
+		photo: 'https://www.edmsauce.com/wp-content/uploads/2017/06/Ghastly.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Sullivan King',
+		photo: 'https://cdn.shopify.com/s/files/1/0464/9525/products/sk2_740x.jpg?v=1542523453',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
